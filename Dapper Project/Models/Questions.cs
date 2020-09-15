e58e7e10bb37e355b0dc90dcbf96ce4d9529621d
Tyler Satkowiak <tylersatkowiak@quickenloans.com>
2020-09-15 15:28:01 -0400

    YO

---
+++ Dapper Project/Models/Questions.cs
@@ -0,0 +1,26 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper_Project.Models
+{
+    [Table("")]
+    public class Questions
+    {   
+        [Key]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        
+        public string Posted { get; set; }
+        //Datetime^
+        public string Category { get; set; }
+        public int Status { get; set; }
+
+
+
+    }
+}
